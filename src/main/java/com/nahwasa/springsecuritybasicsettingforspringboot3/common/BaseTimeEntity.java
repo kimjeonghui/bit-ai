ae6a2124cdaf4a82c9794625da2a10e8a0f04d70
kimjoenghui <20190896@sungshin.ac.kr>
2024-01-05 09:15:15 +0900

    공통

--- src/main/java/com/nahwasa/springsecuritybasicsettingforspringboot3/common/BaseTimeEntity.java
+++ src/main/java/com/nahwasa/springsecuritybasicsettingforspringboot3/common/BaseTimeEntity.java
@@ -2,15 +2,16 @@ package com.nahwasa.springsecuritybasicsettingforspringboot3.common;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
-
     @CreatedDate
     private LocalDateTime createdAt;
 }
